71b774bad65a54a769fb4a7a402b3e6f3a9b7e2c
Bailie Whetten <bailiewhetten@gmail.com>
2026-01-15 00:14:13 -0700

    Initial commit

---
+++ DiceRoll.cs
@@ -0,0 +1,30 @@
+namespace Mission2Assignment;
+
+internal class DiceRoll
+{
+    
+    // Generate 2 Random Numbers - return array of dice roll combinations
+    public int[] Roll(int tempRolls)
+    {
+        // initialize instance of Random class
+        Random rnd = new Random();
+        
+        // initialize variables
+        int dice1;
+        int dice2;
+        int diceCombination;
+        int[] rollLog = new int[13];
+
+        // loop to roll dice per user input
+        for (int i = 0; i < tempRolls; i++)
+        {
+            dice1 = rnd.Next(1, 7);
+            dice2 = rnd.Next(1, 7);
+            diceCombination = dice1 + dice2; //should be a number between 2-12
+            rollLog[diceCombination]++; // increment count in the spot that = the diceCombination
+        }
+        
+        // return array of roll combinations
+        return rollLog;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Mission2Assignment;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // initialize variables
+        string userInput = "";
+        int numRolls;
+        int[] rollArray;
+        double onePercent;
+        double numStars;
+        
+        // Allow user to indicate how many times they want to roll the dice
+        System.Console.WriteLine("Welcome to the dice throwing simulator!");
+        System.Console.WriteLine("How many dice rolls would you like to simulate? ");
+        userInput = System.Console.ReadLine();
+        numRolls = int.Parse(userInput);
+        
+        // create DiceRoll instance w/ user input
+        DiceRoll dr = new DiceRoll();
+        
+        // run Roll method, return array of roll combinations (rollArray)
+        rollArray = dr.Roll(numRolls);
+        // numRolls divided by 100 is 1% of the total rolls
+        onePercent = numRolls / 100.0;
+
+        System.Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
+        System.Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+        System.Console.WriteLine("Total number of rolls = " + userInput + ".\n");
+
+        // Print histogram by iterating through roll combination array (rollArray)
+        for (int i = 2; i < 13; i++)
+        {
+            string stars = "";
+            // calculate percentage per roll combination
+            numStars = Math.Round(rollArray[i] / onePercent);
+            for (int j = 0; j < numStars; j++)
+            {
+                stars += "*";
+            }
+            System.Console.WriteLine(i + ": " + stars);
+        }
+
+        // Exit message
+        System.Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!");
+    }
+}
